b2282a72b303d3cbad76e986a30a7b803d920a03
Alan Berman <alan.berman.92@gmail.com>
2021-09-13 16:36:28 +0100

    initial commit (!)

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Game from "./components/Game";
+import Overview from "./components/Overview";
+import useChosenWord from "./hooks/useChosenWord";
+import useChosenWordSimple from "./hooks/useChosenWordSimple";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        <Overview />
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+// ...
+// guesses
+// <GuessList {guesses} />
